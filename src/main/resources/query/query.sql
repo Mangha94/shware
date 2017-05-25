19511f6f3ab20cb27d5e9d1057ad7febbdbd44f1
Lsh <lshs19@nate.com>
2017-05-25 18:08:03 +0900

    position,spot 수정 추가 완 script 문제-(삭제)

--- src/main/resources/query/query.sql
+++ src/main/resources/query/query.sql
@@ -17,14 +17,15 @@
 --   departmentName VARCHAR (255) DEFAULT NULL,
 -- )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
 
-CREATE TABLE positionManagement (
+
+CREATE TABLE positionData (
   positionNo int(11) NOT NULL AUTO_INCREMENT,
   ranking int(11) DEFAULT NULL,
   positionName varchar(50) DEFAULT NULL,
   PRIMARY KEY (positionNo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-CREATE TABLE spotManagement (
+CREATE TABLE spotData (
   spotNo int(11) NOT NULL AUTO_INCREMENT,
   ranking int(11) DEFAULT NULL,
   spotName varchar(50) DEFAULT NULL,
